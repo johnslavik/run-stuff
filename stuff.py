ece09b629c0a9bdaa8e062f5db864ebe80c9dc2f
Bartosz Sławecki <bartosz@ilikepython.com>
2025-11-08 15:06:20 +0100

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,37 +1,22 @@
-import os
-import subprocess
+import pprint
 import sys
-import tempfile
-from contextlib import contextmanager
-startup_code = "def foo():\n    1/0\n"
+from pathlib import Path
 
+import typeshed_client
 
-@contextmanager
-def new_startup_env(*, code: str, histfile: str = ".pythonhist"):
-    """Create environment variables for a PYTHONSTARTUP script in a temporary directory."""
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filename = os.path.join(tmpdir, "pythonstartup.py")
-        with open(filename, "w") as f:
-            f.write(os.linesep.join(code.splitlines()))
-        yield {"PYTHONSTARTUP": filename, "PYTHON_HISTORY": os.path.join(tmpdir, histfile)}
+context = typeshed_client.SearchContext(
+    typeshed=(typeshed := Path("./typeshed/stdlib")),
+    search_path=[],
+    version=sys.version_info[:2],
+    platform="linux"
+)
 
+stdlib_modules = set(sys.stdlib_module_names)
 
-with new_startup_env(code=startup_code) as startup_env:
-    p = subprocess.Popen(
-        [os.path.join(os.path.dirname(sys.executable), "<stdin>"), "-q", "-i"],
-        env=os.environ | startup_env,
-        executable=sys.executable,
-        text=True,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
-    p.stdin.write("foo()")
-    p.stdin.close()
-    data = p.stdout.read()
-    p.stdout.close()
-    # try to cleanup the child so we don't appear to leak when running
-    # with regrtest -R.
-    p.wait()
-    subprocess._cleanup()
-    print(data)
+for full_name, path in typeshed_client.get_all_stub_files(search_context=context):
+    name, nested, rest = full_name.partition(".")
+    if nested:
+        continue
+    stdlib_modules.discard(name)
+
+pprint.pprint(stdlib_modules)
