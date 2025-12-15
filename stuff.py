279811f0f1786b95a1a2bfa12faa2a3c8bf44829
Bartosz Sławecki <bartosz@ilikepython.com>
2025-12-16 00:44:36 +0100

    This way?

--- stuff.py
+++ stuff.py
@@ -5,8 +5,10 @@ import weakref
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 loop.run_until_complete(asyncio.sleep(0))
-loopwr = weakref.ref(loop)
+loopwr = weakref.ref(loop, lambda wr: print(gc.get_referrers(wr)))
 
 del asyncio, loop
 
-print(gc.get_referrers(loopwr()))
+gc.collect()
+gc.collect()
+gc.collect()
