b0eb35de2da521dd3c981b36a65228b56bb86ca5
Bartosz Sławecki <bartosz@ilikepython.com>
2025-12-16 00:40:29 +0100

    Run Python!

--- stuff.py
+++ stuff.py
@@ -1,3 +1,12 @@
 import asyncio
-asyncio.set_event_loop(loop := asyncio.new_event_loop())
-loop.run_until_complete(asyncio.sleep(1))
+import gc
+import weakref
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+loop.run_until_complete(asyncio.sleep(0))
+loopwr = weakref.ref(loop)
+
+del asyncio, loop
+
+print(gc.get_referrers(loopwr()))
