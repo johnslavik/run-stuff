09a199062b43b93d99f57aa85842c661a91fa77b
Bartosz Sławecki <bartosz@ilikepython.com>
2025-12-16 00:47:17 +0100

    Like this?

--- stuff.py
+++ stuff.py
@@ -1,5 +1,6 @@
 import asyncio
 import gc
+import sys
 import weakref
 
 loop = asyncio.new_event_loop()
@@ -7,7 +8,9 @@ asyncio.set_event_loop(loop)
 loop.run_until_complete(asyncio.sleep(0))
 loopwr = weakref.ref(loop, lambda wr: print(gc.get_referrers(wr)))
 
-del asyncio, loop, loopwr
+del asyncio, loop
+del sys.modules['asyncio']
+del sys.modules['_asyncio']
 
 gc.collect()
 gc.collect()
